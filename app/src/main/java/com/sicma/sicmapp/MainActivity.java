aadc1f1c9eb313600e097e92c4743a88d00f1801
Daniel Morgado Jara <dan.morg.j@gmail.com>
2017-04-23 23:06:25 +0200

    Hi

--- app/src/main/java/com/sicma/sicmapp/MainActivity.java
+++ app/src/main/java/com/sicma/sicmapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // En que rama estoy
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
