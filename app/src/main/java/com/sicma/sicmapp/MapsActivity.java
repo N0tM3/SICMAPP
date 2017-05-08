3bfe413cbbce882e275d8f501f1f07242f91239e
Jose Manuel Rodríguez <joselmenda@gmail.com>
2017-05-08 20:12:42 +0200

    Visitas anadidas!!

--- app/src/main/java/com/sicma/sicmapp/MapsActivity.java
+++ app/src/main/java/com/sicma/sicmapp/MapsActivity.java
@@ -30,7 +30,7 @@ import java.util.ArrayList;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private LatLngBounds MADRID = new LatLngBounds(new LatLng(40.434814, -3.683919400000036), new LatLng(40.434814, -3.683919400000036));
+    private LatLngBounds MADRID = new LatLngBounds(new LatLng(40.4222453,-3.7016385), new LatLng(40.4222453,-3.7016385));
     ArrayList<MarkerOptions> marcadores = new ArrayList<>();
 
     @Override
@@ -60,7 +60,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void ponerMarcadores(GoogleMap googleMap) {
         mMap = googleMap;
         LatLng museoprado = new LatLng(40.4137818, -3.6921270999999933);
-        LatLng unicarlos = new LatLng(40.3317753, -3.766986299999985);
+        LatLng sanagustinbetan = new LatLng(40.4319696, -3.691550799999959);
         LatLng reinasofia = new LatLng(40.4079123, -3.6945568999999523);
         LatLng bellasartes = new LatLng(40.4183042, -3.6965333000000555);
         LatLng plazacallao = new LatLng(40.4201134, -3.705888800000025);
@@ -69,6 +69,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng ucomplutense = new LatLng(40.447825, -3.728587);
         LatLng upm = new LatLng(40.448637, -3.71928);
         LatLng cinedore = new LatLng(40.411676, -3.699073);
+        LatLng albeniz=new LatLng(40.417066,-3.712296);
+        LatLng plazacastilla=new LatLng(40.465536,-3.688597);
+        LatLng canalisabel=new LatLng(40.439124,-3.7008069999999407);
+        LatLng cedex=new LatLng(40.3929345,-3.7049173000000337);
+        LatLng altoarenal=new LatLng(40.3897726,-3.6452193999999736);
+        LatLng telefe=new LatLng(40.419765,-3.7487290000000257);
+        LatLng eduardotorr=new LatLng(40.4723136,-3.6717512999999826);
+        LatLng faromoncloa=new LatLng(40.437304,-3.7216728999999305);
+        LatLng tormentas=new LatLng(40.3559,-3.622690000000034);
+        LatLng paseocastellana=new LatLng(40.4791118,-3.686590199999955);
+        LatLng aranjuez=new LatLng(40.0364203,-3.608865499999979);
+        LatLng atocha=new LatLng(40.4070519,-3.6913500000000568);
+        LatLng wanda=new LatLng(40.4361737,-3.599310599999967);
+        LatLng hipodromozarzuela=new LatLng(40.4685005,-3.757951999999932);
+        LatLng madridrio=new LatLng(40.3980331,-3.710934599999973);
+        LatLng emt=new LatLng(40.4759299,-3.6836779000000206);
 
         BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.mipmap.marcador1);
         BitmapDescriptor icon2 = BitmapDescriptorFactory.fromResource(R.mipmap.marcador2);
@@ -79,10 +95,75 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         markerPrado.icon(icon);
         mMap.addMarker(markerPrado);
 
-        MarkerOptions markerUniCarlos = new MarkerOptions().position(unicarlos).title("Eventos en el salón Agustín Betancourt");
-        markerUniCarlos.snippet(getResources().getString(R.string.agustin));
-        markerUniCarlos.icon(icon2);
-        mMap.addMarker(markerUniCarlos);
+        MarkerOptions markerCedex = new MarkerOptions().position(cedex).title("Visita!!!");
+        markerCedex.snippet(getResources().getString(R.string.cedex));
+        markerCedex.icon(icon2);
+        mMap.addMarker(markerCedex);
+
+        MarkerOptions markerAltoArenal = new MarkerOptions().position(altoarenal).title("Visita!!!");
+        markerAltoArenal.snippet(getResources().getString(R.string.puestodecontrol));
+        markerAltoArenal.icon(icon);
+        mMap.addMarker(markerAltoArenal);
+
+        MarkerOptions markerteleferico = new MarkerOptions().position(telefe).title("Visita!!!");
+        markerteleferico.snippet(getResources().getString(R.string.teleferico));
+        markerteleferico.icon(icon2);
+        mMap.addMarker(markerteleferico);
+
+        MarkerOptions markeredu = new MarkerOptions().position(eduardotorr).title("Visita!!!");
+        markeredu.snippet(getResources().getString(R.string.eduardo));
+        markeredu.icon(icon);
+        mMap.addMarker(markeredu);
+
+        MarkerOptions markerfaromoncloa = new MarkerOptions().position(faromoncloa).title("Visita!!!");
+        markerfaromoncloa.snippet(getResources().getString(R.string.faromoncloa));
+        markerfaromoncloa.icon(icon3);
+        mMap.addMarker(markerfaromoncloa);
+
+        MarkerOptions markertormentas = new MarkerOptions().position(tormentas).title("Visita!!!");
+        markertormentas.snippet(getResources().getString(R.string.estanquedetormentas));
+        markertormentas.icon(icon);
+        mMap.addMarker(markertormentas);
+
+        MarkerOptions markerPaseoCastellana = new MarkerOptions().position(paseocastellana).title("Visita!!!");
+        markerPaseoCastellana.snippet(getResources().getString(R.string.torrespacio));
+        markerPaseoCastellana.icon(icon2);
+        mMap.addMarker(markerPaseoCastellana);
+
+        MarkerOptions markerAranjuez = new MarkerOptions().position(aranjuez).title("Visita!!!");
+        markerAranjuez.snippet(getResources().getString(R.string.aranjuez));
+        markerAranjuez.icon(icon);
+        mMap.addMarker(markerAranjuez);
+
+        MarkerOptions markerAtocha = new MarkerOptions().position(atocha).title("Visita!!!");
+        markerAtocha.snippet(getResources().getString(R.string.atocha));
+        markerAtocha.icon(icon3);
+        mMap.addMarker(markerAtocha);
+
+        MarkerOptions markerWanda = new MarkerOptions().position(wanda).title("Visita!!!");
+        markerWanda.snippet(getResources().getString(R.string.estadiowanda));
+        markerWanda.icon(icon);
+        mMap.addMarker(markerWanda);
+
+        MarkerOptions markerZarzuela = new MarkerOptions().position(hipodromozarzuela).title("Visita!!!");
+        markerZarzuela.snippet(getResources().getString(R.string.zarzuela));
+        markerZarzuela.icon(icon2);
+        mMap.addMarker(markerZarzuela);
+
+        MarkerOptions markermadridRio = new MarkerOptions().position(madridrio).title("Visita!!!");
+        markermadridRio.snippet(getResources().getString(R.string.madridrio));
+        markermadridRio.icon(icon3);
+        mMap.addMarker(markermadridRio);
+
+        MarkerOptions markeremt = new MarkerOptions().position(emt).title("Visita!!!");
+        markeremt.snippet(getResources().getString(R.string.madridrio));
+        markeremt.icon(icon);
+        mMap.addMarker(markeremt);
+
+        MarkerOptions markerSanAgustin = new MarkerOptions().position(sanagustinbetan).title("Eventos en el salón Agustín Betancourt");
+        markerSanAgustin.snippet(getResources().getString(R.string.agustin));
+        markerSanAgustin.icon(icon2);
+        mMap.addMarker(markerSanAgustin);
 
         MarkerOptions markerreinasofia = new MarkerOptions().position(reinasofia).title("Eventos en la carpa reina sofia");
         markerreinasofia.snippet(getResources().getString(R.string.reinasofia));
@@ -124,8 +205,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         markercinedore.icon(icon3);
         mMap.addMarker(markercinedore);
 
+        MarkerOptions markeralbeniz = new MarkerOptions().position(albeniz).title("Eventos en la fundacion Albeniz");
+        markeralbeniz.snippet(getResources().getString(R.string.albeniz));
+        markeralbeniz.icon(icon);
+        mMap.addMarker(markeralbeniz);
+
+        MarkerOptions markerplazacastilla = new MarkerOptions().position(plazacastilla).title("Eventos en la plaza Castilla");
+        markerplazacastilla.snippet(getResources().getString(R.string.plazacastilla));
+        markerplazacastilla.icon(icon2);
+        mMap.addMarker(markerplazacastilla);
+
+        MarkerOptions markercanalisabel = new MarkerOptions().position(canalisabel).title("Eventos en el canal isabel 2");
+        markercanalisabel.snippet(getResources().getString(R.string.canalisabel));
+        markercanalisabel.icon(icon3);
+        mMap.addMarker(markercanalisabel);
+
         marcadores.add(markerPrado);
-        marcadores.add(markerUniCarlos);
+        marcadores.add(markerSanAgustin);
         marcadores.add(markerreinasofia);
         marcadores.add(markerbellasartes);
         marcadores.add(markercinedore);
