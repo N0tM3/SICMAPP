e7e1a73e48fffb4d3ab10b8cb3bcce96d74d1824
Daniel Morgado Jara <dan.morg.j@gmail.com>
2017-05-18 21:47:53 +0200

    Chang in the search arrow

--- app/src/main/java/com/sicma/sicmapp/Activitys/PlaceDetails.java
+++ app/src/main/java/com/sicma/sicmapp/Activitys/PlaceDetails.java
@@ -1,11 +1,13 @@
 package com.sicma.sicmapp.Activitys;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -34,6 +36,15 @@ public class PlaceDetails extends AppCompatActivity {
         imgImagen = (ImageView) findViewById(R.id.ImgPlace);
         tvTitle = (Toolbar) findViewById(R.id.mToolbar);
         tvdescripcion = (TextView) findViewById(R.id.mText);
+        /*
+        ImageView mButton = (ImageView) findViewById(R.id.mShareImg);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareWhith();
+            }
+        });
+        */
     }
 
     /**
@@ -43,6 +54,17 @@ public class PlaceDetails extends AppCompatActivity {
         tvTitle.setTitle(nombre);
         tvdescripcion.setText(descripcion);
         Glide.with(PlaceDetails.this).load(imgUrl).centerCrop().into(imgImagen);
+    }
+
+    /**
+     * Method to share with varios plataforms
+     */
+    public void shareWhith(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT,descripcion);
+        shareIntent.putExtra(Intent.EXTRA_STREAM,imgUrl);
+        shareIntent.setType("image/*");
+        startActivity(Intent.createChooser(shareIntent,nombre));
     }
 
     /**
